c90aa10e79d42c5ba6a6cbec44c141cc8b6ee534
Marion Barré <marionb@bam.tech>
2020-03-19 22:10:42 +0100

    inaccessibleValues

--- src/examples/__tests__/toTest.test.ts
+++ src/examples/__tests__/toTest.test.ts
@@ -1,14 +1,16 @@
 import * as mockdate from "mockdate";
 import * as moment from "moment";
-import { computeDueDateAndSendMail } from "@src/examples/toTest";
+import { Mailer } from "@src/examples/toTest";
 import { sendMail } from "@src/libs/mails";
 import { fakeData } from "@src/fixtures/email";
 import { constructMail } from "@src/libs/mailConstructor";
+import { attachProps } from "@src/libs/attachProps";
 
 mockdate.set("2019-11-10T10:00:00.00Z");
 
 jest.mock("@src/libs/mails");
 jest.mock("@src/libs/mailConstructor");
+jest.mock("@src/libs/attachProps");
 
 describe("[Module] mail", () => {
   beforeEach(() => {
@@ -17,16 +19,30 @@ describe("[Module] mail", () => {
 
   const email = "monEmail";
   const date = moment().add(3, "days");
-  it("should call sendMal", async () => {
-    const resolvedValue = await computeDueDateAndSendMail(email);
-    expect(resolvedValue).toEqual(fakeData);
+  it("should call sendMal with initial value", async () => {
+    const mailer = new Mailer();
+    mailer.email = email;
+    // expect(resolvedValue).toEqual(fakeData);
+    expect(sendMail()).toHaveBeenCalledWith(
+      "initEmail",
+      date,
+      expect.any(Function)
+    );
+    expect(constructMail).toHaveBeenCalledWith();
+  });
+  it("should call sendMal with new value", async () => {
+    const mailer = new Mailer();
+    mailer.email = email;
+    // expect(resolvedValue).toEqual(fakeData);
     expect(sendMail()).toHaveBeenCalledWith(email, date, expect.any(Function));
     expect(constructMail).toHaveBeenCalledWith();
   });
   it("should throw error when sendMAil throws", async () => {
     (sendMail() as jest.Mock).mockRejectedValueOnce(new Error("ERROR"));
     try {
-      await computeDueDateAndSendMail(email);
+      const mailer = new Mailer();
+      mailer.email = email;
+      // await computeDueDateAndSendMail(email);
     } catch (error) {
       expect(error.message).toEqual("ERROR");
     }
--- src/examples/toTest.ts
+++ src/examples/toTest.ts
@@ -1,13 +1,22 @@
 import * as moment from "moment";
 import { sendMail } from "../libs/mails";
 import { constructMail } from "@src/libs/mailConstructor";
+import { attachProps } from "@src/libs/attachProps";
 
-export const computeDueDateAndSendMail = async (
-  email: string
-): Promise<string> => {
+const computeDueDateAndSendMail = async (email: string): Promise<string> => {
   const dueDate = moment().add(3, "days");
   const reconstructBody = () => {
     constructMail();
   };
   return await sendMail()(email, dueDate, reconstructBody);
 };
+
+export class Mailer {
+  email: string = "initEmail";
+
+  constructor() {
+    attachProps(async () => {
+      return await computeDueDateAndSendMail(this.email);
+    }); // observer on props, send Mail when this.email changes
+  }
+}

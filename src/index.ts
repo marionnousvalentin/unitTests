1e80f8f7be1b23f22a7284516d7b11399d18757d
Marion Barré <marionb@bam.tech>
2020-04-09 17:01:33 +0200

    script

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+const testDebugger = (variable: boolean) => {
+  console.log("dedzdezdez");
+  if (variable) {
+    console.log("variable", variable);
+  }
+  console.log("dedzedrqaaaaa");
+};
+
+testDebugger(true);

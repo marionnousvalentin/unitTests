c0b0f8f627cc95de53ef2247bf31cb47b88567e5
Marion Barré <marionb@bam.tech>
2020-04-10 09:20:29 +0200

    ts-node

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
+// @ts-ignore
+import * as express from "express";
+const app = express();
+const port = 3000;
+
+// @ts-ignore
+app.get("/", (req, res) => res.send("Hello World!"));
+
+app.listen(port, () =>
+  console.log(`Example app listening at http://localhost:${port}`)
+);
+
 const testDebugger = (variable: boolean) => {
   console.log("dedzdezdez");
   if (variable) {
